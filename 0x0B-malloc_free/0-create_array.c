4ccb8bbb38951d649dd800f5e4e82755cfdeabce
olamideolayemi <olamid15@gmail.com>
2023-10-04 13:30:27 +0100

    create array updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,12 @@ char *create_array(unsigned int size, char c)
 {
 	char *arr;
 	unsigned int i;
-	
+
 	if (size == 0)
 		return (NULL);
 
 	arr = malloc(sizeof(char) * size);
-	
+
 	if (arr == NULL)
 		return (NULL);
 
