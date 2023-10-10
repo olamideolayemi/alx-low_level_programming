7e195dac138de4faef508eeb3a1f9a5a3916bfce
olamideolayemi <olamid15@gmail.com>
2023-10-10 08:14:34 -0700

    Poopy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - structure of element
+ * @name: name of element
+ * @age: element age
+ * @owner: element owner
+ */
 struct dog
 {
 	char *name;
