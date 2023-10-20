92ca26dcead01b396b9b66e5d12d8fe2b227f4e5
olamideolayemi <olamid15@gmail.com>
2023-10-19 19:22:59 -0700

    update 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-section .data
-    format db 'Hello, Holberton',10,0
-
-section .text
-    global main
-    extern printf
-
+	global main
+	extern printf
 main:
-    push rdi
-    mov rdi, format
-    call printf
-    pop rdi
-
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+	mov edi, format
+	xor eax, eax
+	call printf
+	mov eax, 0
+	ret
+format: db `Hello, Holderton\n`,0
