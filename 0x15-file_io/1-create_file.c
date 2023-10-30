0e09b6a6f9640b7748ea4a24a1edb461d9f4be57
olamideolayemi <olamid15@gmail.com>
2023-10-30 11:59:49 -0700

    create_file corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,20 +14,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	num_written = write(fd, text_content, text_len);
-
 	if (text_content != NULL)
 	{
 		for (text_len = 0; text_content[text_len];)
 			text_len++;
 	}
 
-	if (num_written != text_len)
-	{
-		close(fd);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	num_written = write(fd, text_content, text_len);
+
+	if (fd == -1 || num_written == -1)
 		return (-1);
-	}
+
 	close(fd);
 	return (1);
 }
